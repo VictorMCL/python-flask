2b90363c5d06be1bdfd4a8e4b69eafaee9d6c930
Victor Capote <victor.capote@techandsolve.com>
2020-02-14 08:06:25 -0500

    Prueba

--- src/logic/app.py
+++ src/logic/app.py
@@ -8,26 +8,5 @@ def sumar(a=1,b=2):
     c = a+b
     return str(c)
 
-@app.route('/restar/<a>/<b>') 
-def restar(a=2,b=1):
-    a=int(a)
-    b=int(b)
-    c = a-b
-    return str(c)
-
-@app.route('/multiplicar/<a>/<b>')
-def multiplicar(a=2,b=3):
-    a=int(a)
-    b=int(b)
-    c = a*b
-    return str(c)
-
-@app.route('/dividir/<a>/<b>') 
-def dividir(a=6,b=2):
-    a=int(a)
-    b=int(b)
-    c = a/b
-    return str(c)
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
